1bacf6204c73ac2bb8618948234b8c5608bc5847
Nick Doiron <ndoiron@mapmeld.com>
2017-04-30 23:13:33 -0700

    include vote outcomes

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 2016 districts in GeoJSON format via
 <a href='https://github.com/Schwanksta/2016-congressional-districts'>Ken Schwencke's repo</a>.
 
+Candidate names and vote counts from New York Times and Wikipedia
+
 ## License
 
 MIT License
--- RenderMap.js
+++ RenderMap.js
@@ -12,14 +12,20 @@ var svg = d3.select("#map").append("svg")
   .attr("width", width)
   .attr("height", height);
 
-function renderDistrict(number) {
+function renderDistrict(number, voteInfo) {
   d3.json("topojson/pa-" + number + ".topojson", function(error, district) {
     if (error) {
       throw error;
     }
+
+    var party = "democrat";
+    if (voteInfo.republican && (!voteInfo.democrat || voteInfo.democrat.count < voteInfo.republican.count)) {
+      party = "republican";
+    }
+
     svg.insert("path")
       .datum(topojson.feature(district, district.objects['pa-' + number]))
-      .attr("class", "district")
+      .attr("class", "district " + party)
       .attr("d", path)
       .on("click", function() {
         d3.selectAll(".district").style("fill", "#fc3c2f");
@@ -28,6 +34,15 @@ function renderDistrict(number) {
   });
 }
 
-for (var d = 1; d <= 18; d++) {
-  renderDistrict(d);
-}
+var votesByDistrict = {};
+
+d3.json("vote-totals.json", function(err, districts) {
+  if (err) {
+    throw err;
+  }
+  votesByDistrict = districts;
+
+  for (var d = 1; d <= 18; d++) {
+    renderDistrict(d, districts[d + ""]);
+  }
+});
